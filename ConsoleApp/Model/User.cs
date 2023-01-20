1abd91fde04f9b16b231156576ac19f2c3a6ed4b
Mr EZAD <mr.hgpx@gmail.com>
2023-01-20 14:23:20 +0330

    Add project files.

---
+++ ConsoleApp/Model/User.cs
@@ -0,0 +1,12 @@
+﻿namespace ConsoleApp.Model
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public byte Age { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Family { get; set; }
+        public string Mobile { get; set; }
+    }
+}
